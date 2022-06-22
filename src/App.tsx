7b3562d864b9ca909e7929cb1b984c83e7640009
Shogo Goto <gotoadmn0605@gmail.com>
2022-06-22 21:32:55 +0900

    bbb

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-impot ContentApi from "api/ContentApi"
+import ContentApi from "./api/ContentApi"
 
 
 function App() {
