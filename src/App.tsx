bde7e77e10ab666608968a5e39074ddfede4557a
Shogo Goto <gotoadmn0605@gmail.com>
2022-06-22 21:42:27 +0900

    ccc

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit2 <code>src/App.tsx</code> and save to reload.
+          Edit2 <code>src/App.tsx</code> and save to reload {message}.
         </p>
         {/*
         <a
