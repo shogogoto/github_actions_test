b3f39797ba93e1c41dbed1b493bd29533eff4510
Shogo Goto <gotoadmn0605@gmail.com>
2022-06-22 21:23:39 +0900

    aaa

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+impot ContentApi from "api/ContentApi"
+
 
 function App() {
+  const message = ContentApi.get();
+
   return (
     <div className="App">
       <header className="App-header">
